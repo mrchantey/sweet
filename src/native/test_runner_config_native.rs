3352727b09d66a48bfad562e9d1bc56ae1ccdbed
Peter Hayman <peteyhayman@gmail.com>
2024-09-08 18:31:16 +1000

    release: 0.2.0-rc.2

--- src/native/test_runner_config_native.rs
+++ src/native/test_runner_config_native.rs
@@ -4,25 +4,23 @@ use clap::ArgMatches;
 use glob::Pattern;
 
 impl TestRunnerConfig {
-	///Errors on malformed glob pattern.
-	pub fn from_arg_matchers(value: &ArgMatches) -> Result<Self> {
-		let watch = value.get_flag("watch");
-		let parallel = value.get_flag("parallel");
-		let silent = value.get_flag("silent");
-		let matches = value
-			.get_many::<String>("match")
-			.unwrap_or_default()
-			.map(|s| Pattern::new(&format!("*{s}*")))
-			.collect::<Vec<_>>()
-			.into_iter()
-			.collect::<Result<Vec<_>, _>>()?;
-		Ok(Self {
-			watch,
-			parallel,
-			matches,
-			silent,
-		})
-	}
+    ///Errors on malformed glob pattern.
+    pub fn from_arg_matchers(value: &ArgMatches) -> Result<Self> {
+        let watch = value.get_flag("watch");
+        let parallel = value.get_flag("parallel");
+        let silent = value.get_flag("silent");
+        let matches = value
+            .get_many::<String>("match")
+            .unwrap_or_default()
+            .map(|s| Pattern::new(&format!("*{s}*")))
+            .collect::<Result<Vec<_>, _>>()?;
+        Ok(Self {
+            watch,
+            parallel,
+            matches,
+            silent,
+        })
+    }
 }
 
 // pub fn from_cli_args() -> Self {
