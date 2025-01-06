49989462b7503d0405d9390ff0c015cf332db026
Peter Hayman <peteyhayman@gmail.com>
2025-01-06 14:57:03 +1100

    feat: panic_to_error

--- cli/src/test_runners/deno.ts
+++ cli/src/test_runners/deno.ts
@@ -6,16 +6,13 @@ import init from './bindgen.js'
 // THIS FILE IS CACHED
 // During development, delete target/sweet/deno.ts in between changes
 
-console.log('\nhi from deno')
-
 // cwd will be relative to the crate, ie
 // cargo test --workspace is different cwd from cargo test -p my_crate
 // so we try to use absolute dir
 const sweet_root = Deno.env.get('SWEET_ROOT') || Deno.cwd();
-console.dir(sweet_root);
 
 // wrapper function to avoid abort
-globalThis.__wbg_test_invoke = f => f();
+globalThis.panic_to_error = f => f();
 globalThis.read_file = (path: string) => {
 	let full_path = `${sweet_root}/${path}`
 	try {
