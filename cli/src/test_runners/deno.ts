3c031967bf6dbdbd0d0bb04cd4599df952a55c5e
Peter Hayman <peteyhayman@gmail.com>
2025-01-06 13:51:19 +1100

    feat: sweet root

--- cli/src/test_runners/deno.ts
+++ cli/src/test_runners/deno.ts
@@ -6,13 +6,22 @@ import init from './bindgen.js'
 // THIS FILE IS CACHED
 // During development, delete target/sweet/deno.ts in between changes
 
+console.log('\nhi from deno')
+
+// cwd will be relative to the crate, ie
+// cargo test --workspace is different cwd from cargo test -p my_crate
+// so we try to use absolute dir
+const sweet_root = Deno.env.get('SWEET_ROOT') || Deno.cwd();
+console.dir(sweet_root);
+
 // wrapper function to avoid abort
 globalThis.__wbg_test_invoke = f => f();
 globalThis.read_file = (path: string) => {
+	let full_path = `${sweet_root}/${path}`
 	try {
-		return Deno.readTextFileSync(path)
+		return Deno.readTextFileSync(full_path)
 	} catch (err) {
-		return null	
+		return null
 	}
 };
 
--- cli/src/test_runners/test_wasm.rs
+++ cli/src/test_runners/test_wasm.rs
@@ -1,10 +1,9 @@
 use anyhow::Result;
 use clap::Parser;
 use std::fs;
+use std::path::PathBuf;
 use std::process::Command;
-
-const DENO_RUNNER_PATH: &str = "./target/sweet/deno.ts";
-
+use std::str::FromStr;
 
 /// The wasm test runner
 ///
@@ -47,7 +46,7 @@ impl TestWasm {
 	fn run_wasm_bindgen(&self) -> Result<()> {
 		let status = Command::new("wasm-bindgen")
 			.arg("--out-dir")
-			.arg("./target/sweet")
+			.arg(sweet_target_dir())
 			.arg("--out-name")
 			.arg("bindgen")
 			.arg("--target")
@@ -73,7 +72,8 @@ impl TestWasm {
 	/// if this is the first time this will also ensure deno is installed
 	/// by running `deno --version`
 	fn init_deno(&self) -> Result<()> {
-		if let Ok(true) = fs::exists(DENO_RUNNER_PATH) {
+		let deno_runner_path = deno_runner_path();
+		if let Ok(true) = fs::exists(&deno_runner_path) {
 			return Ok(());
 		};
 
@@ -86,7 +86,7 @@ impl TestWasm {
 			anyhow::bail!(INSTALL_DENO);
 		}
 		// wasm-bindgen will ensure parent dir exists
-		fs::write(DENO_RUNNER_PATH, include_str!("./deno.ts"))?;
+		fs::write(deno_runner_path, include_str!("./deno.ts"))?;
 		Ok(())
 	}
 
@@ -98,7 +98,8 @@ impl TestWasm {
 		let status = Command::new("deno")
 			.arg("--allow-read")
 			.arg("--allow-net")
-			.arg(DENO_RUNNER_PATH)
+			.arg("--allow-env")
+			.arg(deno_runner_path())
 			.args(args)
 			.status()?;
 
@@ -109,6 +110,22 @@ impl TestWasm {
 	}
 }
 
+fn workspace_root() -> PathBuf {
+	let root_str = std::env::var("SWEET_ROOT").unwrap_or_default();
+	PathBuf::from_str(&root_str).unwrap()
+}
+
+fn sweet_target_dir() -> PathBuf {
+	let mut path = workspace_root();
+	path.push("target/sweet");
+	path
+}
+
+fn deno_runner_path() -> PathBuf {
+	let mut path = sweet_target_dir();
+	path.push("deno.ts");
+	path
+}
 
 const INSTALL_DENO: &str = "
 🦖 Sweet uses Deno for WASM tests 🦖
@@ -119,3 +136,18 @@ powershell: 	irm https://deno.land/install.ps1 | iex
 other: 				https://docs.deno.com/runtime/getting_started/installation/
 
 ";
+
+
+#[cfg(test)]
+mod test {
+	use crate::prelude::*;
+	use sweet::prelude::*;
+	use test_wasm::deno_runner_path;
+
+	#[test]
+	fn works() {
+		expect(deno_runner_path().to_string_lossy())
+			.to_end_with("target/sweet/deno.ts");
+		expect(true).to_be_false();
+	}
+}
