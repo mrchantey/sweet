030e2252c8eb9e0c1bfa6c88ce932a7345f48ac8
Peter Hayman <peteyhayman@gmail.com>
2025-02-01 13:06:43 +1100

    patch: reexport random

--- crates/sweet_bevy/src/resources/random_source.rs
+++ crates/sweet_bevy/src/resources/random_source.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use rand::Rng;
 use rand::SeedableRng;
 use rand_chacha::ChaCha8Rng;
 
@@ -40,10 +41,74 @@ impl Default for RandomSource {
 	}
 }
 
+
+/// save the `use rand::Rng` shenannigans
+impl RandomSource {
+	/// see [Rng::random]
+	pub fn random<T>(&mut self) -> T
+	where
+		rand::distr::StandardUniform: rand::prelude::Distribution<T>,
+	{
+		self.0.random()
+	}
+
+	/// see [Rng::random_iter]
+	pub fn random_iter<T>(
+		self,
+	) -> rand::distr::Iter<rand::distr::StandardUniform, ChaCha8Rng, T>
+	where
+		Self: Sized,
+		rand::distr::StandardUniform: rand::prelude::Distribution<T>,
+	{
+		self.0.random_iter()
+	}
+
+	/// see [Rng::random_range]
+	pub fn random_range<T, R>(&mut self, range: R) -> T
+	where
+		T: rand::distr::uniform::SampleUniform,
+		R: rand::distr::uniform::SampleRange<T>,
+	{
+		self.0.random_range(range)
+	}
+
+	/// see [Rng::random_bool]
+	pub fn random_bool(&mut self, p: f64) -> bool { self.0.random_bool(p) }
+
+	/// see [Rng::random_ratio]
+	pub fn random_ratio(&mut self, numerator: u32, denominator: u32) -> bool {
+		self.0.random_ratio(numerator, denominator)
+	}
+
+	/// see [Rng::sample]
+	pub fn sample<T, D: rand::prelude::Distribution<T>>(
+		&mut self,
+		distr: D,
+	) -> T {
+		self.0.sample(distr)
+	}
+
+	/// see [Rng::sample_iter]
+	pub fn sample_iter<T, D>(
+		self,
+		distr: D,
+	) -> rand::distr::Iter<D, ChaCha8Rng, T>
+	where
+		D: rand::prelude::Distribution<T>,
+		Self: Sized,
+	{
+		self.0.sample_iter(distr)
+	}
+
+	/// see [Rng::fill]
+	pub fn fill<T: rand::Fill + ?Sized>(&mut self, dest: &mut T) {
+		self.0.fill(dest)
+	}
+}
+
 #[cfg(test)]
 mod test {
 	use crate::prelude::*;
-	use rand::Rng;
 	use sweet_test::prelude::*;
 
 
